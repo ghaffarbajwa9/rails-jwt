84388bee50545450c31408bc609c09b43548e166
ghaffarbajwa9 <ghaffarbajwa9@gmail.com>
2024-05-22 02:19:25 +0500

    jwt token is added, user and todo model and controller added, routes established

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
+
+  resources :todos
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
